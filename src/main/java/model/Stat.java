eae389522d04b1144310a406f1750d9f643e15c0
Aaron Van Curen <99290929+vancurenaaron@users.noreply.github.com>
2023-10-12 06:58:44 -0400

    Created TableView method. Updated view methods.

--- src/main/java/model/Stat.java
+++ src/main/java/model/Stat.java
@@ -2,6 +2,9 @@ package model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -67,8 +70,8 @@ public class Stat
             {
                 int statId = result.getInt(1);
                 String name = result.getString(2);
-                int effect = result.getInt(1);
-                String description = result.getString(2);
+                int effect = result.getInt(3);
+                String description = result.getString(4);
                 stats.add(new Stat(statId, name, effect, description));
             }
         }
@@ -82,5 +85,49 @@ public class Stat
         }
 
         return stats;
+    }
+
+    public static ObservableList<Stat> viewAllStats(int characterId)
+    {
+        ObservableList<Stat> stats = FXCollections.observableArrayList();
+        try
+        {
+            PreparedStatement ps = connection.prepareStatement(QueryBuilder.viewAllStatsQuery());
+            ResultSet result = ps.executeQuery();
+            while (result.next())
+            {
+                int statId = result.getInt(1);
+                String name = result.getString(2);
+                int effect = result.getInt(3);
+                String description = result.getString(4);
+                stats.add(new Stat(statId, name, effect, description));
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Failed to retrieve all skills. Error: " + new RuntimeException(e).getMessage());
+        }
+        catch (Exception e)
+        {
+            System.out.println("Failed to retrieve all skills. Error: " + e.getMessage());
+        }
+
+        return stats;
+    }
+
+    public static TableView<Stat> getTableView()
+    {
+        TableView<Stat> tableView = new TableView<>();
+        TableColumn<Stat, String> name = new TableColumn<>("Name");
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn<Stat, String> effect = new TableColumn<>("Effect");
+        effect.setCellValueFactory(new PropertyValueFactory<>("effect"));
+        TableColumn<Stat, String> description = new TableColumn<>("Description");
+        description.setCellValueFactory(new PropertyValueFactory<>("description"));
+        tableView.getColumns().add(name);
+        tableView.getColumns().add(effect);
+        tableView.getColumns().add(description);
+        tableView.getItems().addAll(Stat.viewAllStats());
+        return tableView;
     }
 }
