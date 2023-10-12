f9ca75d3aace325ff0a7448f42fea4fba2649125
Aaron Van Curen <99290929+vancurenaaron@users.noreply.github.com>
2023-10-12 06:55:04 -0400

    Updated several queries.

--- src/main/java/model/QueryBuilder.java
+++ src/main/java/model/QueryBuilder.java
@@ -5,36 +5,39 @@ public class QueryBuilder
     public static String createCharacterQuery()
     {
         // TODO: Need to account for any linked tables
-        return " INSERT INTO character" + " (name, experienceNeeded, availableSkillPoints, description, level, experience)" + " VALUES" + " ( ?, ?, ?, ?, ?, ? ) ";
+        return " INSERT INTO `character` (name, description, level, experience, experienceNeeded, availableSkillPoints) VALUES ( ?, ?, 1, 0, 1000, 0 ) ";
     }
 
     public static String viewAllCharactersQuery()
     {
-        // TODO: Need to account for any linked tables
-        return " SELECT * FROM character ";
+        return " SELECT * FROM `character` ";
     }
 
     public static String viewCharacterQuery()
     {
-        // TODO: Need to account for any linked tables
-        return " SELECT * FROM character WHERE characterId = ? ";
+        return " SELECT * FROM `character` WHERE characterId = ? ";
     }
 
     public static String updateCharacterQuery()
     {
         // TODO: Need to account for any linked tables
-        return " UPDATE character" + " (characterId, name, experienceNeeded, availableSkillPoints, description, level, experience)" + " VALUES ( ?, ?, ?, ?, ?, ? )" + " WHERE characterId = ? ";
+        return " UPDATE `character` (characterId, name, experienceNeeded, availableSkillPoints, description, level, experience) VALUES ( ?, ?, ?, ?, ?, ? )" + " WHERE characterId = ? ";
     }
 
     public static String deleteCharacterQuery()
     {
         // TODO: Need to account for any linked tables
-        return " DELETE FROM character WHERE characterId = ? ";
+        return " DELETE FROM `character` WHERE characterId = ? ";
     }
 
     public static String viewAllSkillsQuery()
     {
-        return "SELECT * FROM skill";
+        return " SELECT * FROM skill sk JOIN `rank` rk ON rk.skillId = sk.skillId JOIN stat st ON rk.statId = st.statId ";
+    }
+
+    public static String viewAllCharacterSkillsQuery()
+    {
+        return " select" + " sk.skillId as skillId," + " sk.name as skillName," + " sk.category as skillCategory," + " sk.tier as skillTier," + " cs.skillRank as `rank`" + " from character_skill cs" + " join skill sk on cs.skillId = sk.skillId" + " where cs.characterId = ? ";
     }
 
     public static String viewAllStatsQuery()
