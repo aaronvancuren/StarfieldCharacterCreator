dd752fd3210a59505a58df0c7a744e4821af7294
Aaron Van Curen <99290929+vancurenaaron@users.noreply.github.com>
2023-10-12 06:56:10 -0400

    Updated the getContent method. Implemented the use of TableViews. Worked on logic for Create/Update character.

--- src/main/java/view/menu.java
+++ src/main/java/view/menu.java
@@ -1,8 +1,6 @@
 package view;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleListProperty;
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -17,7 +15,6 @@ import model.StarfieldCharacter;
 import model.Stat;
 
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.Optional;
 
 // TODO: Add documentation
@@ -26,30 +23,54 @@ import java.util.Optional;
 public class menu extends Application
 {
     private StarfieldCharacter character;
-    private LinkedList<StarfieldCharacter> characters;
-    private TextField tfCharacterName;
-    private TextField tfCharacterLevel;
-    private ProgressBar pgCharacterExperience;
-    private TextField tfCharacterSkillPoints;
-    private TextArea taCharacterDescription;
-    private ListView<Skill> lvCharacterSkills;
-    private ListView<Stat> lvCharacterStats;
+    private TableView<StarfieldCharacter> tvCharacters = new TableView<>();
+    private TableView<Skill> tvCharacterSkills = new TableView<>();
+    private TableView<Stat> tvCharacterStats = new TableView<>();
 
     @Override
     public void start(Stage stage) throws IOException
     {
-        HBox crudButtons = getCrudButtons();
-        GridPane gridForm = getGridForm();
+        stage.setTitle("Starfield Character Creator");
+        stage.getIcons().add(new Image("file:src/main/images/starfield.png"));
+        stage.setScene(new Scene(getContent()));
+        stage.setMinWidth(510);
+        stage.show();
+    }
+
+    private VBox getContent()
+    {
+        tvCharacters = StarfieldCharacter.getTableView();
+        tvCharacters.setMinWidth(500);
+        tvCharacters.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        Label lblCharacters = new Label("Characters", tvCharacters);
+        lblCharacters.setContentDisplay(ContentDisplay.BOTTOM);
+
+        tvCharacterSkills = Skill.getTableView();
+        tvCharacterSkills.setMinWidth(500);
+        tvCharacterSkills.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        Label lblCharacterSkills = new Label("Skills", tvCharacterSkills);
+        lblCharacterSkills.setContentDisplay(ContentDisplay.BOTTOM);
+
+        tvCharacterStats = Stat.getTableView();
+        tvCharacterStats.setMinWidth(500);
+        Label lblCharacterStats = new Label("Stats", tvCharacterStats);
+        lblCharacterStats.setLabelFor(tvCharacterStats);
+        lblCharacterStats.setContentDisplay(ContentDisplay.BOTTOM);
+
+        GridPane gpForm = new GridPane();
+        gpForm.setPadding(new Insets(5));
+        gpForm.setHgap(15);
+        gpForm.setVgap(15);
+        gpForm.add(lblCharacters, 0, 1);
+        gpForm.add(getCrudButtons(), 1, 1);
+        gpForm.add(lblCharacterSkills, 0, 2);
+        gpForm.add(lblCharacterStats, 1, 2);
 
         VBox root = new VBox();
         root.setFillWidth(true);
-        root.setPadding(new Insets(20));
-        root.getChildren().addAll(crudButtons, gridForm);
-
-        stage.setTitle("Starfield Character Creator");
-        stage.getIcons().add(new Image("file:src/main/images/starfield.png"));
-        stage.setScene(new Scene(root));
-        stage.show();
+        root.setPadding(new Insets(5));
+        root.getChildren().addAll(gpForm);
+        return root;
     }
 
     private HBox getCrudButtons()
@@ -57,144 +78,82 @@ public class menu extends Application
         Button btnCreateCharacter = new Button("Create Character");
         btnCreateCharacter.setOnAction(e -> createCharacter());
 
-        Button btnViewCharacters = new Button("View Characters");
-        btnViewCharacters.setOnAction(e -> viewCharacter());
-
-        Button btnUpdateCharacters = new Button("Update Characters");
+        Button btnUpdateCharacters = new Button("Update Character");
         btnUpdateCharacters.setOnAction(e -> updateCharacter());
 
-        Button btnDeleteCharacters = new Button("Delete Characters");
+        Button btnDeleteCharacters = new Button("Delete Character");
         btnDeleteCharacters.setOnAction(e -> deleteCharacter());
 
-        HBox crudButtons = new HBox(10, btnCreateCharacter, btnViewCharacters, btnUpdateCharacters,
+        HBox crudButtons = new HBox(5, btnCreateCharacter, btnUpdateCharacters,
                                     btnDeleteCharacters);
-        crudButtons.setPadding(new Insets(10));
+
+        crudButtons.setSpacing(30);
         crudButtons.setAlignment(Pos.BASELINE_CENTER);
         return crudButtons;
     }
 
-    private GridPane getGridForm()
-    {
-        tfCharacterName = new TextField();
-        tfCharacterName.setMaxWidth(255);
-        Label lblCharacterName = new Label("Name: ");
-        lblCharacterName.setLabelFor(tfCharacterName);
-        lblCharacterName.setContentDisplay(ContentDisplay.RIGHT);
-
-        tfCharacterLevel = new TextField("1");
-        tfCharacterLevel.setEditable(false);
-        tfCharacterLevel.setMaxWidth(25);
-        Label lblCharacterLevel = new Label("Level: ");
-        lblCharacterLevel.setLabelFor(tfCharacterLevel);
-        lblCharacterLevel.setContentDisplay(ContentDisplay.RIGHT);
-
-        pgCharacterExperience = new ProgressBar(0);
-        Label lblCharacterExperience = new Label("Experience: ");
-        lblCharacterExperience.setLabelFor(pgCharacterExperience);
-        lblCharacterExperience.setContentDisplay(ContentDisplay.RIGHT);
-        lblCharacterExperience.setTooltip(new Tooltip("0 / 1000 exp points"));
-
-        tfCharacterSkillPoints = new TextField("3");
-        tfCharacterSkillPoints.setEditable(false);
-        tfCharacterSkillPoints.setMaxWidth(25);
-        Label lblCharacterSkillPoints = new Label("Skill Points: ");
-        lblCharacterSkillPoints.setLabelFor(tfCharacterSkillPoints);
-        lblCharacterSkillPoints.setContentDisplay(ContentDisplay.RIGHT);
-
-
-        taCharacterDescription = new TextArea();
-        taCharacterDescription.setWrapText(true);
-        taCharacterDescription.setMaxWidth(255);
-        Label lblCharacterDescription = new Label("Description: ");
-        lblCharacterDescription.setLabelFor(taCharacterDescription);
-        lblCharacterDescription.setContentDisplay(ContentDisplay.RIGHT);
-
-
-        // TODO: Create List/Table View
-        lvCharacterSkills = new ListView<>(Skill.viewAllSkills());
-        lvCharacterSkills.setMaxWidth(255);
-        lvCharacterSkills.setMaxHeight(255);
-        lvCharacterSkills.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        Label lblCharacterSkills = new Label("Skills", lvCharacterSkills);
-        lblCharacterSkills.setContentDisplay(ContentDisplay.BOTTOM);
-
-        // TODO: Create List/Table View
-        lvCharacterStats = new ListView<>(Stat.viewAllStats());
-        lvCharacterStats.setMaxWidth(255);
-        lvCharacterStats.setMaxHeight(255);
-        Label lblCharacterStats = new Label("Stats", lvCharacterStats);
-        lblCharacterStats.setLabelFor(lvCharacterStats);
-        lblCharacterStats.setContentDisplay(ContentDisplay.BOTTOM);
-
-        GridPane gpForm = new GridPane();
-        gpForm.setPadding(new Insets(10));
-        gpForm.setHgap(10);
-        gpForm.setVgap(10);
-        gpForm.addRow(1, lblCharacterName, tfCharacterName, lblCharacterLevel, tfCharacterLevel, lblCharacterExperience,
-                      pgCharacterExperience, lblCharacterSkillPoints, tfCharacterSkillPoints);
-        gpForm.addRow(2, lblCharacterDescription, taCharacterDescription);
-        gpForm.add(lblCharacterSkills, 1, 3);
-        gpForm.add(lblCharacterStats, 2, 3, 6, 1);
-        return gpForm;
-    }
-
     private void createCharacter()
     {
-        if (tfCharacterName.getText().isEmpty())
+        try
         {
-            Alert alert = new Alert(Alert.AlertType.WARNING, "Character name is required.");
-            alert.show();
-            return;
-        }
-
-        if (taCharacterDescription.getText().isEmpty())
-        {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to give your character a description?");
-            alert.getButtonTypes().clear();
-            alert.getButtonTypes().add(new ButtonType("Yes", ButtonBar.ButtonData.YES));
-            alert.getButtonTypes().add(new ButtonType("No", ButtonBar.ButtonData.NO));
-            Optional<ButtonType> result = alert.showAndWait();
-            if (result.isPresent() && result.get().getButtonData() == ButtonBar.ButtonData.YES)
+            Dialog<StarfieldCharacter> dialog = new CharacterDialog();
+            Optional<StarfieldCharacter> result = dialog.showAndWait();
+            if (result.isPresent())
             {
-                return;
+                character = result.get();
+                if (character.getCharacterId() != -1)
+                {
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Character was created.");
+                    alert.show();
+                }
             }
         }
-
-        ObservableList<Skill> skills = lvCharacterSkills.getSelectionModel().getSelectedItems();
-        if (skills.isEmpty() || Integer.parseInt(tfCharacterSkillPoints.textProperty().get()) > 0)
+        catch (Exception e)
         {
-            Alert alert = new Alert(Alert.AlertType.WARNING,
-                                    "Must select three (3) new skills for your character. Only " + tfCharacterSkillPoints.textProperty()
-                                                                                                                         .get() + " have been selected.");
-            alert.show();
-            return;
+            System.out.println(e.getMessage());
         }
-
-        // TODO: Need method to create default stat values.
-
-        character = new StarfieldCharacter(tfCharacterName.getText(), taCharacterDescription.getText(), skills,
-                                           new SimpleListProperty<>());
-        StarfieldCharacter.createCharacter(character);
-    }
-
-    private void viewAllCharacter()
-    {
-        characters = StarfieldCharacter.viewAllCharacter();
-        CharacterDialog test = new CharacterDialog(characters);
-    }
-
-    private void viewCharacter()
-    {
-        StarfieldCharacter.viewCharacter(character.getCharacterId());
     }
 
     private void updateCharacter()
     {
-        StarfieldCharacter.updateCharacter(character);
+        try
+        {
+            StarfieldCharacter selectedCharacter = tvCharacters.getSelectionModel().getSelectedItem();
+            if (selectedCharacter == null)
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Must select a character.");
+                alert.show();
+                return;
+            }
+
+            Dialog<StarfieldCharacter> dialog = new CharacterDialog(selectedCharacter);
+            Optional<StarfieldCharacter> result = dialog.showAndWait();
+            if (result.isPresent())
+            {
+                character = result.get();
+                if (character.getCharacterId() != -1)
+                {
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Character was updated.");
+                    alert.show();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 
     private void deleteCharacter()
     {
-        StarfieldCharacter.deleteCharacter(character.getCharacterId());
+        StarfieldCharacter selectedCharacter = tvCharacters.getSelectionModel().getSelectedItem();
+        if (selectedCharacter == null)
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Must select a character.");
+            alert.show();
+            return;
+        }
+
+        StarfieldCharacter.deleteCharacter(selectedCharacter.getCharacterId());
     }
 }
