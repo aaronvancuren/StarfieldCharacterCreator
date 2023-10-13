af18cdc726587c6daff356d1fed0d369c1dc2216
Aaron Van Curen <99290929+vancurenaaron@users.noreply.github.com>
2023-10-13 14:04:41 -0400

    created all necessary queries to perform oprerations for the application

--- src/main/java/model/QueryBuilder.java
+++ src/main/java/model/QueryBuilder.java
@@ -2,6 +2,7 @@ package model;
 
 public class QueryBuilder
 {
+    //#region Character Queries
     public static String createCharacterQuery()
     {
         return " INSERT INTO `character` (name, description, level, experience, experienceNeeded, availableSkillPoints) VALUES ( ?, ?, 1, 0, 1000, 0 ) ";
@@ -19,17 +20,19 @@ public class QueryBuilder
 
     public static String updateCharacterQuery()
     {
-        return " UPDATE `character` (characterId, name, experienceNeeded, availableSkillPoints, description, level, experience) VALUES ( ?, ?, ?, ?, ?, ? )" + " WHERE characterId = ? ";
+        return " UPDATE `character` (characterId, name, experienceNeeded, availableSkillPoints, description, level, experience) VALUES ( ?, ?, ?, ?, ?, ? ) WHERE characterId = ? ";
     }
 
     public static String deleteCharacterQuery()
     {
         return " DELETE FROM `character` WHERE characterId = ? ";
     }
+    //#endregion
 
-    public static String viewAllSkillsQuery()
+    //#region Character Skill Quereies
+    public static String createCharacterSkillsQuery()
     {
-        return " SELECT * FROM skill sk" + " JOIN `rank` rk ON rk.skillId = sk.skillId" + " JOIN stat st ON rk.statId = st.statId ";
+        return " INSERT INTO character_skill (characterId, skillId, skillRank, rankProgress, nextRankAvailable) VALUES (?, ?, ?, 0, 0) "
     }
 
     public static String viewCharacterSkillsQuery()
@@ -37,13 +40,43 @@ public class QueryBuilder
         return " SELECT sk.skillId AS skillId, sk.name AS skillName, sk.category AS skillCategory," + " sk.tier AS skillTier, cs.skillRank AS `rank` from character_skill cs" + "join skill sk on cs.skillId = sk.skillId" + " where cs.characterId = ? ";
     }
 
-    public static String viewAllStatsQuery()
+    public static String updateCharacterSkillsQuery()
     {
-        return "SELECT * FROM stat";
+        return " UPDATE character_skill SET skillRank = ?, rankProgress = ?, nextRankAvailable = ? WHERE characterId = ? AND skillId = ? "
+    }
+
+    public static String deleteCharacterSkillsQuery()
+    {
+        return " DELETE FROM character_skill WHERE characterId = ? "
+    }
+    //#endregion
+
+    //#region Character Stat Queries
+    public static String createCharacterStats()
+    {
+        return "INSERT INTO character_stat (characterId, statId) VALUES (?, ?)"
     }
 
     public static String viewCharacterStatsQuery()
     {
         return " SELECT st.statId AS statId, st.name AS name, st.effect AS effect, st.description AS description" + " FROM character_stat cst JOIN stat st ON cst.statId = st.statId WHERE cst.characterId = ? ";
     }
+
+    public static String deleteCharaterStatsQuery()
+    {
+        return " DELETE FROM character_stat cs JOIN stat st ON cs.statId = st.statId WHERE characterId = ? "
+    }
+    //#endregion
+
+    //#region ReadOnly
+    public static String viewAllSkillsQuery()
+    {
+        return " SELECT * FROM skill sk" + " JOIN `rank` rk ON rk.skillId = sk.skillId" + " JOIN stat st ON rk.statId = st.statId ";
+    }
+
+    public static String viewAllStatsQuery()
+    {
+        return " SELECT * FROM stat ";
+    }
+    //#endregion
 }
