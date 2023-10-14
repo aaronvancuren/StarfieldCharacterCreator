6ef2dd9fbb9a1ecc197fda4625f1588d67018eff
Aaron Van Curen <99290929+vancurenaaron@users.noreply.github.com>
2023-10-13 20:43:51 -0400

    add documentation to query builders

--- src/main/java/model/QueryBuilder.java
+++ src/main/java/model/QueryBuilder.java
@@ -2,78 +2,137 @@ package model;
 
 public class QueryBuilder
 {
+    /**
+     * Query for creating character base data
+     *
+     * @return Query string
+     */
     //#region Character Queries
     public static String createCharacterQuery()
     {
         return " INSERT INTO `character` (name, description, level, experience, experienceNeeded, availableSkillPoints) VALUES ( ?, ?, 1, 0, 1000, 0 ) ";
     }
 
+    /**
+     * Query for retrieving all characters base data
+     * @return Query string
+     */
     public static String viewAllCharactersQuery()
     {
         return " SELECT * FROM `character` ";
     }
 
+    /**
+     * Query for retrieving all character base data
+     * @return Query string
+     */
     public static String viewCharacterQuery()
     {
         return " SELECT * FROM `character` WHERE characterId = ? ";
     }
 
+    /**
+     * Query for updating all character base data
+     * @return Query string
+     */
     public static String updateCharacterQuery()
     {
         return " UPDATE `character` (characterId, name, description, level, experience, experienceNeeded, availableSkillPoints) VALUES ( ?, ?, ?, ?, ?, ? ) WHERE characterId = ? ";
     }
 
+    /**
+     * Query for deleting all character base data
+     * @return Query string
+     */
     public static String deleteCharacterQuery()
     {
         return " DELETE FROM `character` WHERE characterId = ? ";
     }
     //#endregion
 
-    //#region Character Skill Quereies
+    /**
+     * Query for creating character skills
+     *
+     * @return Query string
+     */
+    //#region Character Skill Queries
     public static String createCharacterSkillsQuery()
     {
-        return " INSERT INTO character_skill (characterId, skillId, skillRank, rankProgress, nextRankAvailable) VALUES (?, ?, ?, 0, 0) "
+        return " INSERT INTO character_skill (characterId, skillId, skillRank, rankProgress, nextRankAvailable) VALUES (?, ?, ?, 0, 0) ";
     }
 
+    /**
+     * Query for retrieving character skills
+     * @return Query string
+     */
     public static String viewCharacterSkillsQuery()
     {
         return " SELECT sk.skillId AS skillId, sk.name AS skillName, sk.category AS skillCategory," + " sk.tier AS skillTier, cs.skillRank AS `rank` from character_skill cs" + "join skill sk on cs.skillId = sk.skillId" + " where cs.characterId = ? ";
     }
 
+    /**
+     * Query for updating character skills
+     * @return Query string
+     */
     public static String updateCharacterSkillsQuery()
     {
-        return " UPDATE character_skill SET skillRank = ?, rankProgress = ?, nextRankAvailable = ? WHERE characterId = ? AND skillId = ? "
+        return " UPDATE character_skill SET skillRank = ?, rankProgress = ?, nextRankAvailable = ? WHERE characterId = ? AND skillId = ? ";
     }
 
+    /**
+     * Query for deleting all character skills
+     * @return Query string
+     */
     public static String deleteCharacterSkillsQuery()
     {
-        return " DELETE FROM character_skill WHERE characterId = ? "
+        return " DELETE FROM character_skill WHERE characterId = ? ";
     }
     //#endregion
 
+    /**
+     * Query for creating character stats
+     * @return Query string
+     */
     //#region Character Stat Queries
     public static String createCharacterStatsQuery()
     {
-        return "INSERT INTO character_stat (characterId, statId) VALUES (?, ?)"
+        return "INSERT INTO character_stat (characterId, statId) VALUES (?, ?)";
     }
 
+    /**
+     * Query for retrieving all character stats
+     * @return Query string
+     */
     public static String viewCharacterStatsQuery()
     {
         return " SELECT st.statId AS statId, st.name AS name, st.effect AS effect, st.description AS description" + " FROM character_stat cst JOIN stat st ON cst.statId = st.statId WHERE cst.characterId = ? ";
     }
 
-    public static String deleteCharaterStatsQuery()
+    /**
+     * Query for deleting all character stats
+     *
+     * @return Query string
+     */
+    public static String deleteCharacterStatsQuery()
     {
-        return " DELETE FROM character_stat cs JOIN stat st ON cs.statId = st.statId WHERE characterId = ? "
+        return " DELETE FROM character_stat cs JOIN stat st ON cs.statId = st.statId WHERE characterId = ? ";
     }
     //#endregion
 
+    /**
+     * Query for viewing all skills
+     * @return Query string
+     */
     //#region ReadOnly
     public static String viewAllSkillsQuery()
     {
         return " SELECT * FROM skill sk" + " JOIN `rank` rk ON rk.skillId = sk.skillId" + " JOIN stat st ON rk.statId = st.statId ";
     }
 
+    /**
+     * Query for viewing all stats
+     * @return Query string
+     */
     public static String viewAllStatsQuery()
     {
         return " SELECT * FROM stat ";
